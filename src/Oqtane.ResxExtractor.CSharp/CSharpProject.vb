9d69e2448ca0208cef5ad4b8b7b79bd680237be2
hishamco <hishamco_2007@yahoo.com>
2021-03-05 21:20:01 +0300

    Add CSharpProject

---
+++ src/Oqtane.ResxExtractor.CSharp/CSharpProject.vb
@@ -0,0 +1,13 @@
+﻿Imports Oqtane.ResxExtractor.Core
+
+Public Class CSharpProject
+    Inherits Project
+
+    Public Sub New(path As String)
+        MyBase.New(path)
+    End Sub
+
+    Public Overrides ReadOnly Property Extension As String = ".csproj"
+
+    Public Overrides ReadOnly Property FilesExtension As String = ".cs"
+End Class
