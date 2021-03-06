25cfb91d0400a8f9887f19da1117855670e0bf7a
hishamco <hishamco_2007@yahoo.com>
2021-03-06 15:08:00 +0300

    Add LocalizedStringCollection

---
+++ src/Oqtane.ResxExtractor.Core/Extensions/LocalizedStringCollectionExtensions.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module LocalizedStringCollectionExtensions
+    <Extension>
+    Public Sub Add(collection As LocalizedStringCollection, occurence As LocalizedStringOccurence)
+        If collection Is Nothing Then
+            Throw New ArgumentNullException(NameOf(collection))
+        End If
+
+        If occurence Is Nothing Then
+            Throw New ArgumentNullException(NameOf(occurence))
+        End If
+
+        collection.Add(New LocalizedString(occurence))
+    End Sub
+
+    <Extension>
+    Public Sub AddRange(collection As LocalizedStringCollection, occurences As IEnumerable(Of LocalizedStringOccurence))
+        If collection Is Nothing Then
+            Throw New ArgumentNullException(NameOf(collection))
+        End If
+
+        If occurences Is Nothing Then
+            Throw New ArgumentNullException(NameOf(occurences))
+        End If
+
+        For Each occurence As LocalizedStringOccurence In occurences
+            collection.Add(occurence)
+        Next
+    End Sub
+End Module
---
+++ src/Oqtane.ResxExtractor.Core/LocalizedStringCollection.vb
@@ -0,0 +1,59 @@
+﻿Public Class LocalizedStringCollection
+    Implements ICollection(Of LocalizedString)
+
+    Private ReadOnly _localizedStringsDictionary As IDictionary(Of String, LocalizedString)
+
+    Public Sub New()
+        _localizedStringsDictionary = New Dictionary(Of String, LocalizedString)()
+    End Sub
+
+    Public ReadOnly Property Count As Integer Implements ICollection(Of LocalizedString).Count
+        Get
+            Return _localizedStringsDictionary.Values.Count
+        End Get
+    End Property
+
+    Public ReadOnly Property IsReadOnly As Boolean Implements ICollection(Of LocalizedString).IsReadOnly
+        Get
+            Return _localizedStringsDictionary.Values.IsReadOnly
+        End Get
+    End Property
+
+    Public Sub Add(item As LocalizedString) Implements ICollection(Of LocalizedString).Add
+        If item Is Nothing Then
+            Throw New ArgumentNullException(NameOf(item))
+        End If
+
+        Dim key As String = item.Value
+        Dim localizedString As LocalizedString = Nothing
+        If _localizedStringsDictionary.TryGetValue(key, localizedString) Then
+            localizedString.Locations.AddRange(item.Locations)
+        Else
+            _localizedStringsDictionary.Add(key, item)
+        End If
+    End Sub
+
+    Public Sub Clear() Implements ICollection(Of LocalizedString).Clear
+        _localizedStringsDictionary.Clear()
+    End Sub
+
+    Public Sub CopyTo(array() As LocalizedString, arrayIndex As Integer) Implements ICollection(Of LocalizedString).CopyTo
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function Contains(item As LocalizedString) As Boolean Implements ICollection(Of LocalizedString).Contains
+        Return _localizedStringsDictionary.Values.Contains(item)
+    End Function
+
+    Public Function Remove(item As LocalizedString) As Boolean Implements ICollection(Of LocalizedString).Remove
+        Return _localizedStringsDictionary.Remove(item.Value)
+    End Function
+
+    Public Function GetEnumerator() As IEnumerator(Of LocalizedString) Implements IEnumerable(Of LocalizedString).GetEnumerator
+        Return _localizedStringsDictionary.Values.GetEnumerator()
+    End Function
+
+    Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Return GetEnumerator()
+    End Function
+End Class
